35fd8fa38fb3fd63779a9b1f5a9a3b0f2a85bb4c
sjlucn <363306041@qq.com>
2019-05-31 15:46:17 +0800

    Update PlasticPaginator.php
    modify

--- src/PlasticPaginator.php
+++ src/PlasticPaginator.php
@@ -18,11 +18,29 @@ class PlasticPaginator extends LengthAwarePaginator
      * @param int           $limit
      * @param int           $page
      */
+    /*
     public function __construct(PlasticResult $result, $limit, $page)
     {
         $this->result = $result;
 
         parent::__construct($result->hits(), $result->totalHits(), $limit, $page,
+            ['path' => LengthAwarePaginator::resolveCurrentPath()]);
+
+        $hitsReference = &$this->items;
+
+        $result->setHits($hitsReference);
+    }
+    */
+    public function __construct(PlasticResult $result, $limit, $page , $limitPage = 0)
+    {
+        $this->result = $result;
+        $total = $result->totalHits();
+        if($limitPage > 0 && Auth::check() == false && $total/$limit > $limitPage)
+        {
+            $total = $limit * $limitPage;
+        }
+
+        parent::__construct($result->hits(), $total, $limit, $page,
             ['path' => LengthAwarePaginator::resolveCurrentPath()]);
 
         $hitsReference = &$this->items;
